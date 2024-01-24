d38a1c3dade1e1cc25fdd74b2d6c30efa77fca22
Yudai <greatsceneryundermountain@gmail.com>
2024-01-24 13:51:41 +0900

    ヘッダー調整

--- header.php
+++ header.php
@@ -8,6 +8,9 @@
   </head>
   <body>
     <header class="header">
-      <a href="<?php echo home_url('/'); ?>">TOPへ</a>
-      <a href="https://github.com/Yudaiyam/news-site/tree/ver.02" target="_blank" rel="noopener">GitHubでソースコードを確認</a>
+      <a href="<?php echo home_url('/'); ?>">TOP</a>
+      <div class="h-menu">
+        <a href="https://github.com/Yudaiyam/news-site/tree/ver.02" target="_blank" rel="noopener">GitHubでソースコードを確認</a>
+        <a href="https://classroom.yamashita-develop.site/individual/000_lab" target="_blank" rel="noopener">Ver.01</a>
+      </div>
     </header>
