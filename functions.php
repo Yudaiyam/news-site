e0e4c9ad32f1f0f255ae2313bf2b33dda5d5f0fe
Yudai <greatsceneryundermountain@gmail.com>
2024-03-08 17:27:20 +0900

    各コードの説明追加

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php 
+    // cssファイルの読み込みここから
     add_action( 'wp_enqueue_scripts', function(){
         wp_register_style(
             'reset_style',
@@ -13,10 +14,18 @@
             '1.0'
         );
     });
+    // cssファイルの読み込みここまで
+
+    // サムネイル画像の表示ここから
     function setup_theme() {
         add_theme_support('post-thumbnails');
     }
     add_action('after_setup_theme', 'setup_theme');
+    // サムネイル画像の表示ここまで
+
+    // 管理画面のカスタマイズここから
+    // ・管理画面の「投稿」ラベルを「お知らせ」に変更
+    // ・archive.phpのスラッグを"news"に
     function post_has_archive( $args, $post_type ) {
         if ( 'post' == $post_type ) {
         $args['rewrite'] = true;
@@ -26,4 +35,5 @@
         return $args;
         }
         add_filter( 'register_post_type_args', 'post_has_archive', 10, 2 );
+    // 管理画面のカスタマイズここまで
 ?>
